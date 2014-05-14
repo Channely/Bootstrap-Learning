a6df2a5430988589f45ee6bd167f2bc2ba27acad
Channely <qijie29896@gmail.com>
2014-05-14 17:30:58 +0800

    add a cChinese word to test new deploy  shell script

--- deploy_lesson_by_another_repository.sh
+++ deploy_lesson_by_another_repository.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 #get the commit content by user input
-echo "Please input the commit words :"
+echo "\033[33m Please input the commit words : \033[0m"
 read commit
-echo "the commit content is:'$commit'"
+echo "\033[34m Content:'$commit' \033[0m"
 
 #push code change on this respo.
-echo "now pushing code local"
+echo "\033[33m now pushing code local \033[0m"
 git add .
 git commit -m "${commit}"
 git pull --rebase
 git push
-echo "local end"
+echo "\033[32m local end \033[0m"
 
 #sync deploy project to another repos.
-echo "sync code"
+echo "\033[33m sync code \033[0m"
 rm -rf ../Bootstrap-Learning-Deploy/*
 cp -rf PracticeProject/* ../Bootstrap-Learning-Deploy/
 cd ../Bootstrap-Learning-Deploy/
-echo "now pushing code another"
+echo "\033[33m now pushing code another \033[0m"
 git add .
 git commit -m "${commit}"
 git pull --rebase
 git push
-echo "another end"
+echo "\033[32m another end \033[0m"
 cd ../Bootstrap-Learning/
