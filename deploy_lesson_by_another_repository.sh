2cf3a759baa2aa3a65cc6ea4e28e41749f24b579
Channely <qijie29896@gmail.com>
2014-05-14 17:19:31 +0800

    add a little change on code to test the new sync shell script

--- deploy_lesson_by_another_repository.sh
+++ deploy_lesson_by_another_repository.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
+#get the commit content by user input
+echo "Please input the commit words :"
+read commit
+echo "the commit content is:'$commit'"
+
+#push code change on this respo.
+echo "now pushing code local"
+git add .
+git commit -m "${commit}"
+git pull --rebase
+git push
+echo "local end"
+
+#sync deploy project to another repos.
+echo "sync code"
 rm -rf ../Bootstrap-Learning-Deploy/*
 cp -rf PracticeProject/* ../Bootstrap-Learning-Deploy/
 cd ../Bootstrap-Learning-Deploy/
+echo "now pushing code another"
 git add .
-git commit -m 'update practice code from ../Bootstrap-Learning/PracticeProject/*'
+git commit -m "${commit}"
 git pull --rebase
 git push
+echo "another end"
 cd ../Bootstrap-Learning/
